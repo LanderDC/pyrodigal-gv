4d25f6c0c6f449f899b0027e0a86d3318ffd353b
LanderDC <75493579+LanderDC@users.noreply.github.com>
2024-12-09 15:56:54 +0100

    bump version

--- pyrodigal_gv/__init__.py
+++ pyrodigal_gv/__init__.py
@@ -1,7 +1,7 @@
 """A Pyrodigal extension for improved prediction of viral genes.
 """
 
-__version__ = "0.3.3"
+__version__ = "0.4.0"
 __author__ = "Martin Larralde <martin.larralde@embl.de>"
 __license__ = "GPLv3"
 
